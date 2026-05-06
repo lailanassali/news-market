f57d03f94fa98f1a7747d9891ef513f2d5fd34d6
lailanassali <lailapyt@yahoo.com>
2026-05-06 21:23:20 +0100

    feat: wire up domain selection in HomeScreen

--- NewsMarket/screens/HomeScreen.tsx
+++ NewsMarket/screens/HomeScreen.tsx
@@ -2,32 +2,40 @@ import { FlatList, StyleSheet, Text, View } from "react-native";
 import { DOMAINS } from "../constants";
 import { useContext } from "react";
 import { ArticleContext } from "../context/ArticleContext";
-
+import { DomainPill } from "../components/DomainPill";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export const HomeScreen = () => {
 
-    const { articles } = useContext(ArticleContext);
+    const { articles, selectedDomains, toggleDomain } = useContext(ArticleContext);
 
     return (
-        <View style={styles.container}>
-        <Text>NewsMarket</Text>
-        <FlatList 
-            horizontal
-            data={DOMAINS} // domains from constants
-            keyExtractor={(item) => item}
-            renderItem={({ item }) => (
-                <></> // domain pill component
-            )}
-            showsHorizontalScrollIndicator={false}
-         />
-         <FlatList
-            data={articles} // articles from context
-            keyExtractor={(item) => item.source.id || item.title} 
-            renderItem={({ item }) => (
-                <></> // article card component
-            )}
-         />
-        </View>
+        <SafeAreaView style={styles.container}>
+            <Text style={styles.headingText}>NewsMarket</Text>
+            <Text>Select a domain to read articles</Text>
+            <View>
+                <FlatList 
+                    horizontal
+                    data={DOMAINS} 
+                    keyExtractor={(item) => item}
+                    renderItem={({ item }) => (
+                        <DomainPill isSelected={selectedDomains.includes(item)} domain={item} onPress={() => toggleDomain(item)} /> 
+                    )}
+                    showsHorizontalScrollIndicator={false}
+                    style={styles.flatList}
+                />
+            </View>
+            <View style={styles.articlesContainer}>
+                <Text style={styles.subheadingText}>Latest articles</Text>
+                <FlatList
+                    data={articles} 
+                    keyExtractor={(item) => item.source.id || item.title} 
+                    renderItem={({ item }) => (
+                        <></> // article card component
+                    )}
+                    />
+            </View>
+        </SafeAreaView>
     );
 }
 
@@ -36,5 +44,23 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         padding: 16,
+    },
+    articlesContainer: {
+        flex: 1,    
+    },
+    headingText: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        paddingBottom: 6,
+    },
+    flatList: {
+        marginTop: 16,
+        textAlign: "center"
+    },
+    subheadingText: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        marginBottom: 8,
+        marginTop: 24,
     },
 });
