80b02ede7e28d2b196998f0ab69ae5e10f83342a
lailanassali <lailapyt@yahoo.com>
2026-05-07 15:51:52 +0100

    feat: add SortButton by latest or popopularity component

---
+++ NewsMarket/components/sortButton.tsx
@@ -0,0 +1,39 @@
+import { StyleSheet, Text, TouchableOpacity } from "react-native";
+
+type SortButtonProps = {
+    sortBy: 'publishedAt' | 'popularity';
+    setSortBy: (sort: 'publishedAt' | 'popularity') => void;
+};
+
+export const SortButton: React.FC<SortButtonProps> = ({ sortBy, setSortBy }) => {
+    return (
+        <TouchableOpacity
+            style={styles.button}
+            onPress={() => setSortBy(sortBy === 'publishedAt' ? 'popularity' : 'publishedAt')}
+            accessibilityRole="button"
+            accessibilityLabel={`Sort by ${sortBy === 'publishedAt' ? 'popularity' : 'latest'}`}
+            accessibilityState={{ selected: false }}
+        >
+            <Text style={styles.text}>
+                Sort: {sortBy === 'publishedAt' ? 'Latest' : 'Popular'}
+                 <Text style={{ fontFamily: 'System' }}> ⇅</Text>
+            </Text>
+        </TouchableOpacity>
+    );
+};
+
+const styles = StyleSheet.create({
+    button: {
+        backgroundColor: '#eee',
+        borderRadius: 20,
+    
+        paddingVertical: 6,
+        paddingHorizontal: 12,
+    },
+    text: {
+        fontSize: 12,
+        color: '#333',
+        fontWeight: 'bold',
+
+    },
+});
