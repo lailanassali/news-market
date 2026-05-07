9054702bfc9482853cf44f9f9b53ffea9dc5041f
lailanassali <lailapyt@yahoo.com>
2026-05-07 14:20:43 +0100

    chore: configure jest and typescript for testing

--- NewsMarket/babel.config.js
+++ NewsMarket/babel.config.js
@@ -1,9 +1,3 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
-  plugins: [
-    ['module:react-native-dotenv', {
-      moduleName: '@env',
-      path: '.env',
-    }]
-  ]
 };
